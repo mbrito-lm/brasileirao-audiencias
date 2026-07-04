fbb67c6c134a016f07c594829956e700088541e7
Murilo Medeiros Brito <mbrito@livemode.local>
2026-07-04 00:06:37 -0300

    Add authorized callback to NextAuth middleware to enforce login redirect

--- src/auth.ts
+++ src/auth.ts
@@ -9,14 +9,14 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     }),
   ],
   callbacks: {
+    authorized({ auth, request }) {
+      const isLoggedIn = !!auth?.user;
+      const isLoginPage = request.nextUrl.pathname.startsWith("/login");
+      if (isLoginPage) return true;
+      return isLoggedIn;
+    },
     async signIn({ profile }) {
       return !!profile?.email?.endsWith("@livemode.com");
-    },
-    async session({ session, token }) {
-      return session;
-    },
-    async jwt({ token, profile }) {
-      return token;
     },
   },
   pages: {
